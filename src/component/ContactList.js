75530d8afa8e0d56b175972e5de4509714176cfa
Zahra Mdienh <madinehzahra76@gmail.com>
2022-10-02 19:19:12 +0330

    change ui

--- src/component/ContactList.js
+++ src/component/ContactList.js
@@ -1,29 +1,15 @@
 import '../App.css';
 import { useState } from "react";
-import {FaHeart, FaRegEdit, FaRegHeart, FaRegTrashAlt, FaUserPlus, FaSearch, FaCircle} from "react-icons/fa";
-import {BsGrid} from "react-icons/bs";
+import {FaHeart, FaRegHeart} from "react-icons/fa";
+import Toolbar from "./Toolbar";
+import Message from "./Message";
+import ContactListDetail from "./ContactListDetail";
 
 
-const ContactList = ({contact, setContact, setForm, setFormStatus}) => {
-
-    const [item, setItem] = useState()
-    const [searchFavEnable, setSearchFavEnable] = useState(false)
-
-    const [filter, setFilter] = useState('allcontact')
-    const [search, setSearch] = useState('')
+const ContactList = ({contact, setContact, setForm, setFormStatus, search, searchFavEnable, setShowForm}) => {
 
     const [showDelMessage, setsShowDelMessage] = useState('none')
     const [delId, setDelId] = useState(0)
-
-    const favouriteFilter = e => {
-        //if(e.target.checked)  setSearchFavEnable(true)
-        //else  setSearchFavEnable(false)
-        setSearchFavEnable(!searchFavEnable)
-    }
-
-    const contactFilter = e => {
-        setSearch(e.target.value)
-    }
 
     const handleDelete = () => {
         setContact(contact.filter(c => c.id !== delId))
@@ -45,7 +31,8 @@ const ContactList = ({contact, setContact, setForm, setFormStatus}) => {
     }
 
     const handleUpdate = c => {
-        setFormStatus('update')
+        setShowForm(true)
+        setFormStatus('Update')
         setForm(c)
     }
 
@@ -54,76 +41,40 @@ const ContactList = ({contact, setContact, setForm, setFormStatus}) => {
     }
 
     const showInfo = c => {
-        setFormStatus(false)
+        setShowForm(true)
+        setFormStatus('Show')
         setForm(c)
     }
 
+    return (
+        <div style={{width: '100%', display: 'flex', flexDirection: "column", alignItems: 'center'}}>
 
-return (
-    <div style={{width: '100%', display: 'flex', flexDirection: "column", alignItems: 'center'}}>
-        <div className='toolbar'>
-            <div style={{margin: '10px', width: '30%'}}>
-                {/*<label htmlFor={'search-contact'}>search contacts: </label>*/}
-                <div className={'toolbar-search'}>
-                    <FaSearch style={{color: '#a1a1a1', margin: '0 6px'}}/>
-                    <input className='search_input' name={'search-contact'} type={"text"} onChange={contactFilter} />
-                </div>
+            <Message handleDelete={handleDelete} handleNoDel={handleNoDel} showDelMessage={showDelMessage} />
 
-            </div>
+            <div className={'contactList'}>
+                { contact.filter(c => ((c.name.toUpperCase().includes(search.toUpperCase())) || (c.family.toUpperCase().includes(search.toUpperCase()))) &&
+                    (searchFavEnable ? c.favourite : true)).map(c => (
 
-            <h2 style={{fontSize:'25px', color: '#a1a1a1', margin: '0 10px'}}>Contacts</h2>
-
-            <div className={'toolbarIcon-container'}>
-                {/*<label htmlFor={'search-favourite'}>favourite contacts: </label>*/}
-                {/*<input className='search_input' name={'search-favourite'} type={"checkbox"} onChange={favouriteFilter}/>*/}
-
-                {searchFavEnable ? <FaHeart className={'toolbarFav_icon'} style={{color: 'red'}} onClick={favouriteFilter} /> :
-                    <FaRegHeart className={'toolbarFav_icon'} style={{color: '#a1a1a1'}} onClick={favouriteFilter}/> }
-                <FaUserPlus className={'toolbar-addUser'}/>
-            </div>
-        </div>
-
-        <div style={{display: showDelMessage, alignItems: 'center'}}>
-            <FaCircle style={{color: 'red', marginRight: '10px'}} />
-            <p style={{color: '#525252'}}>Are you sure to delete contact?</p>
-            <button className={'btn'} onClick={handleDelete}>yes</button>
-            <button className={'btn'} onClick={handleNoDel}>no</button>
-        </div>
-
-        <div className={'contactList'}>
-            { contact.filter(c =>
-                ((c.name.toUpperCase().includes(search.toUpperCase())) ||
-                    (c.family.toUpperCase().includes(search.toUpperCase()))) &&
-                (searchFavEnable ? c.favourite : true)).map(c => (
-
-                <div className="contactItem">
-                    <div className='contactContainer'>
-                        <div className='contactItem_image'>
-                            <img src={`https://avatars.dicebear.com/api/${c.gender}/${c.id}.svg`} width={'30px'} height={'30px'}/>
+                    <div className="contactItem">
+                        <div className='contactContainer'>
+                            <div className='contactItem_image'>
+                                <img src={`https://avatars.dicebear.com/api/${c.gender}/${c.id}.svg`} width={'30px'} height={'30px'}/>
+                            </div>
+                            <div className='contactItem_title' onClick={() => showDetails(c.id)}>
+                                <h4>{c.name} {c.family}</h4>
+                                <h6 style={{fontSize: '12px', color: '#8c8c8c', marginTop: '8px'}}>{c.number}</h6>
+                            </div>
+                            <div className='contactItem_favourite' onClick={() => handleFavourite(c.id)}>
+                                {c.favourite ? <FaHeart style={{color: 'red'}}/> : <FaRegHeart />}
+                            </div>
                         </div>
-                        <div className='contactItem_title' onClick={() => showDetails(c.id)}>
-                            <h4>{c.name} {c.family}</h4>
-                        </div>
-                        <div className='contactItem_favourite' onClick={() => handleFavourite(c.id)}>
-                            {c.favourite ? <FaHeart style={{color: 'red'}}/> : <FaRegHeart />}
-                        </div>
+
+                        <ContactListDetail c={c} handleDelMessage={handleDelMessage} handleUpdate={handleUpdate} showInfo={showInfo}/>
                     </div>
-
-                    <div className='wrapper' style={c.enable ? {height: '100%'} : {height: '1px'}}>
-                        <span style={{border: '1px solid #D8D8D8', width: '100%', margin: '10px 0'}}></span>
-                        <div className='iconContainer'>
-                            <BsGrid onClick={() => showInfo(c)}/>
-                            <FaRegTrashAlt onClick={() => handleDelMessage(c.id)}/>
-                            <FaRegEdit onClick={() => handleUpdate(c)}/>
-
-                        </div>
-                    </div>
-                </div>
-            ))}
-
+                ))}
+            </div>
         </div>
-    </div>
-)
+    )
 }
-//contact={searchEnable ? item : contact}
+
 export default ContactList;
