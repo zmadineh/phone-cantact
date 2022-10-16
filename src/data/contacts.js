3babd2cc0d5ac17dfa925cbe73d49604f81c2579
Zahra Mdienh <madinehzahra76@gmail.com>
2022-10-16 16:22:51 +0330

    add redux-toolkit, add contact slice

---
+++ src/data/contacts.js
@@ -0,0 +1,6 @@
+
+export const contacts = [
+    {id: 1, name: 'Zahra', family: 'Madineh', age: '25', gender: 'female', country: 'iran', city: 'isfahan', number:'091111111', email:'zahra@gmail.com', image: `https://avatars.dicebear.com/api/female/1.svg`, favourite: false, enable: false},
+    {id: 2, name: 'Mahdi', family: 'Ahmadi', age: '25', gender: 'male', country: 'iran', city: 'isfahan', number:'092222222', email:'mahdi@gmail.com', image: `https://avatars.dicebear.com/api/male/2.svg`, favourite: true, enable: false},
+    {id: 3, name: 'Ali', family: 'Madineh', age: '25', gender: 'male', country: 'iran', city: 'isfahan', number:'093333333', email:'ali@gmail.com', image: `https://avatars.dicebear.com/api/male/3.svg`, favourite: false, enable: false},
+]
---
+++ src/toolkit/contact.slice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {contacts} from "../data/contacts";
+
+export const contactSlice = createSlice({
+    name: "contact",
+    initialState: { contacts },
+    reducers: {
+        addContact: (state, action) => {
+            const payload = action.payload;
+            const index = state.contacts.findIndex((contact) => contact.number === payload.number);
+            if (index === -1)
+                state.contacts.push(payload);
+        },
+        removeContact: (state, action) => {
+            state.contacts = state.contacts.filter((contact) => contact.id !== action.payload.id);
+        },
+        updateContact: (state, action) => {
+            const payload = action.payload;
+            state.contacts = state.contacts.map((contact) => contact.id === payload.id ? payload : contact);
+        },
+        updateFavfContact: (state, action) => {
+            const payload = action.payload;
+            state.contacts.map((contact) =>  contact.id === payload.id ? {...contact, favourite: !contact.favourite} : contact);
+        },
+        collapseContact: (state, action) => {
+            const payload = action.payload;
+            state.contacts.map((contact) =>  contact.id === payload.id ? {...contact, enable: true} : {...contact, enable: false});
+        }
+    },
+});
+
+export const {
+    addContact,
+    removeContact,
+    updateContact,
+    updateFavfContact,
+    collapseContact,
+} = contactSlice.actions;
+
+export default contactSlice.reducer;
